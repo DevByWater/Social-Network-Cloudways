d6ca67b2dbed8b7704edfe93c4fe6d6ce0f31c56
DevByWater <egaraudy@gmail.com>
2017-01-15 15:43:48 -0700

    added accessors to user model

--- app/User.php
+++ app/User.php
@@ -35,4 +35,12 @@ class User extends Authenticatable
     {
         $this->attributes['password']=bcrypt($value);
     }
+    public function getNameAttribute($value)
+    {
+        return "User: " . $value;
+    }
+    public function getEmailAttribute($value)
+    {
+        return strtok($value, '@');
+    }
 }
