d6da232c9fbc445632b82196a90fb4511732b0ab
DevByWater <egaraudy@gmail.com>
2017-01-22 13:25:37 -0700

    created articles table and controller

--- database/migrations/2017_01_22_201711_create_articles_table.php
+++ database/migrations/2017_01_22_201711_create_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class CreateArticlesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->text('content');
+            $table->boolean('live')->default(0);
+            $table->timestamp('post_on')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('articles');
     }
 }
